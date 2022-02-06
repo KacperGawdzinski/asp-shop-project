016aa731c1f641b3203bdbfa1e8b5b6cb5a2ba64
KacperGawdzinski <gawdzinskikacper@gmail.com>
2022-02-07 00:39:37 +0100

    db update

--- Controllers/ShopController.cs
+++ Controllers/ShopController.cs
@@ -7,19 +7,25 @@ using project.Models;
 
 namespace project.Controllers {
     public class ShopController : Controller {
-        project.Models.Processor[] procs = new project.Models.Processor[] {
-            new project.Models.Processor(){ id = 0, name = "i7 8700k", hash="z1.jpg", price = 900},
-            new project.Models.Processor(){ id = 1, name = "i7 10700k", hash="z2.jpg", price = 1400}
-        };
         // GET: Shop
         public ActionResult Public() {
-            return View(procs);
+            //ViewBag.Login = "chuj";
+            using (var context = new DatabaseDataContext()) {
+               project.Models.Processor[] procs = context.Processor.Select(x => new Models.Processor {
+                    id = x.id,
+                    name = x.name,
+                    hash = x.hash,
+                    price = (double)x.price
+                }).ToArray();
+                return View(procs);
+            }
+            
         }
         public ActionResult Logged() {
-            return View(procs);
+            return View();
         }
         public ActionResult Admin() {
-            return View(procs);
+            return View();
         }
     }
 }
